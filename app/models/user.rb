d364aff395adcdcf10186666dc0d9fd7c01f117e
Romain Bousseau <romainbousseau1@gmail.com>
2017-04-07 16:05:55 +0200

    username form need fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username,
-  :presence => true,
-  :uniqueness => {
-    :case_sensitive => false
-  }
 end

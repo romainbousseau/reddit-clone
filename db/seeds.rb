1414da0c4dd0467ac53afcff3807ee8c8d5cca89
Romain Bousseau <romainbousseau1@gmail.com>
2017-04-07 19:20:24 +0200

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Subject.destroy_all
+User.destroy_all
+
+# Users
+User.new(email: 'javier@psg.com', password: 'azerty', password_confirmation: 'azerty', username: 'Javier').save
+User.new(email: 'hatem@psg.com', password: 'azerty', password_confirmation: 'azerty', username: 'Hatem').save
+User.new(email: 'edinson@psg.com', password: 'azerty', password_confirmation: 'azerty', username: 'Edinson').save
+User.new(email: 'julian@psg.com', password: 'azerty', password_confirmation: 'azerty', username: 'Julian').save
+
+# Subjects
+Subject.new(title: 'Allez Paris SG', content: 'O ville lumière sens la chaleur', user_id: User.find(1).id).save
+Subject.new(title: 'Eh Oh Redstar FC', content: 'Le Red Star c\'est seulement à Bauer', user_id: User.find(2).id).save
+Subject.new(title: 'OM = Sida', content: 'Dans les égouts des rats...', user_id: User.find(3).id).save
+Subject.new(title: 'MFC ohéohé', content: 'Menil en league des champions', user_id: User.find(4).id).save

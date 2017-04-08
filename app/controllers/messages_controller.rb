a4dbffb18de71007ceecfa162da6fadbab87dc72
Romain Bousseau <romainbousseau1@gmail.com>
2017-04-08 17:02:19 +0200

    message edit needs fixing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
-    before_action :set_subject, only: [:new, :create, :edit, :update, :show]
+  before_action :set_subject, only: [:new, :create, :edit, :update, :show]
   def new
     @message = Message.new
   end
@@ -22,6 +22,8 @@ class MessagesController < ApplicationController
   end
 
   def update
+    @message.update(message_params)
+    redirect_to subject_path(@subject)
   end
 
   def destroy
@@ -32,11 +34,12 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   end
 
+  def set_subject
+    @subject = Subject.find(params[:subject_id])
+  end
+
   def message_params
     params.require(:message).permit(:content, :subject_id, :user_id)
   end
 
-    def set_subject
-    @subject = Subject.find(params[:subject_id])
-  end
 end
